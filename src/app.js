376c5d081b9ce099776e313d9707a9297894d298
paul <jp@infinitedev.works>
2018-04-20 17:49:17 +0800

    Added new components

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 import './css/main.scss';
 
 const $ = require('jquery');
-const mainView = require('./main.hbs')
+const mainView = require('./main.hbs');
 
-$('body').append(mainView)
+$('body').append(mainView);
 
